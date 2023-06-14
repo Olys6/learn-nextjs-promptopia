31ef49903257cd30683dc9aebe11bc34dd003340
Oliver S <oliver@olys.tech>
2023-06-14 13:41:55 +0100

    added params to prompt routejs

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -2,12 +2,25 @@ import Prompt from '@models/prompt';
 import { connectToDB } from '@utils/database';
 
 export const GET = async request => {
+	const url = new URL(request.url);
+	const searchParams = url.searchParams;
+	const search = searchParams.get('search') || '';
+	const regex = new RegExp(search, 'i');
+
 	try {
 		await connectToDB();
 
-		const prompts = await Prompt.find({}).populate(
-			'creator'
-		);
+		const prompts = await Prompt.find({
+			prompt: { $regex: regex },
+		}).populate('creator');
+
+		const headers = {
+			'Content-Type': 'application/json',
+			'Cache-Control':
+				'no-store, no-cache, must-revalidate',
+			Expires: '0',
+			Pragma: 'no-cache',
+		};
 
 		return new Response(JSON.stringify(prompts), {
 			status: 200,
